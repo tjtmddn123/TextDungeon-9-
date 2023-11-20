1321d4ff917b67601260c9606de5482e8bef4408
Limseokchang <111727297+PsychoLim@users.noreply.github.com>
2023-11-20 22:00:57 +0900

    Stage1.5
    몬스터를 쉽게 생성할 수 있게 함수를 수정,추가 했습니다

--- TextDungeon/Stage.cs
+++ TextDungeon/Stage.cs
@@ -19,7 +19,7 @@ namespace TextDungeon
     {
         List<string> stage1mapname = new List<string>() { "들판", "들판안쪽", "평야외곽", "호수로가는길", "호수", "호수안쪽", "호수바닥" };
         List<string> stage2mapname = new List<string>() { "가위", "바위", "보", "가나다", "라마바", "사아자", "차카" };
-
+        List<string> stage3mapname = new List<string>() { "가위", "바위", "보", "가나다", "라마바", "사아자", "차카" };
         public int thisstagenum = 1;
 
         int CheckValidInput(int min, int max)
@@ -28,7 +28,6 @@ namespace TextDungeon
             bool result;
             do
             {
-                Console.WriteLine("원하시는 행동을 입력해주세요.");
                 result = int.TryParse(Console.ReadLine(), out keyInput);
             } while (result == false || CheckIfValid(keyInput, min, max) == false);
 
@@ -41,12 +40,12 @@ namespace TextDungeon
             return false;
         }
 
-        public void Stage1(Character _player, List<Monster> monsters, int stagenum) // 함수의 이름만 바꿔서 큰 스테이지만큼 만들면 댐..
+        public void Stage1(Character _player, List<Monster> monsters, int stagenum , int minstage) // 함수의 이름만 바꿔서 큰 스테이지만큼 만들면 댐..
         {
              // 스테이지의 이름 설정을 하기위한 리스트
             int randomRoom = new Random().Next(); // 각 스테이지의 방을 랜덤으로 설정한다. ex) 1 - 1 번방에 몬스터가 나올 수 도 있고 다른 특수한 방이 나올 수 도 있다.
 
-            StageSet(_player, monsters,stagenum,0); // 스테이지 1에 맞는 세팅을 해주는 메서드.
+            StageSet(_player, monsters, stagenum, minstage); // 스테이지 1에 맞는 세팅을 해주는 메서드.
 
             // * 몬스터는 스테이지 고정, 개체수 랜덤, 1 ~ 4 *
 
@@ -57,13 +56,12 @@ namespace TextDungeon
 
 
             // 도핑식으로 포션을 먹을지 말지 선택.
-
             //BattleScene(_player, monster);
             //BattleScene(_player, monster);
             //BattleScene(_player, monster);
             //BattleScene(_player, monster);
             //BattleScene(_player, monster);
-            //BattleScene(_player, monster); 
+            //BattleScene(_player, monster);
             Thread.Sleep(1000);
             Console.Clear();
 
@@ -74,7 +72,7 @@ namespace TextDungeon
         public void StageSet(Character _player, List<Monster> monsters, int stagenum, int minstage) // 스테이지에 따라 몬스터를 다르게 배치할 수 있도록, 여기서 세팅한다.
         {
 
-            int randomIncount = new Random().Next(1, 4); // 여기서 각 스테이지의 몬스터 수를 랜덤하게 설정한다.
+            int randomIncount = new Random().Next(1, 4); // 각 스테이지의 몬스터 수를 랜덤하게 설정한다.
 
             switch (stagenum)
             {
@@ -83,14 +81,149 @@ namespace TextDungeon
                     {
                         case 0:
                             //만약 1-0 방이라면~ 몬스터 세팅을 어떻게 
+                            for (int i = 0; i < randomIncount; i++)
+                            {
+                                monsters.Add(new Goblin());
+                            } // 스테이지 마다 분리해서 
                             break;
+                        case 1: // 1 - 1
+                            for (int i = 0; i < randomIncount; i++)
+                            {
+                                monsters.Add(new Goblin()); // 슬라임
+                            } // 스테이지 마다 분리해서 
+                            break;
+                        case 2: // 1 - 2
+                            for (int i = 0; i < randomIncount; i++)
+                            {
+                                monsters.Add(new Goblin());
+                            } // 스테이지 마다 분리해서 
+                            break;
+                        case 3: // 1 - 3                             
+                            for (int i = 0; i < randomIncount; i++)
+                            {
+                                monsters.Add(new Goblin());
+                            } // 스테이지 마다 분리해서 
+                            break;
+                        case 4: // 1 - 4                        
+                            for (int i = 0; i < randomIncount; i++)
+                            {
+                                monsters.Add(new Goblin());
+                            } 
+                            break;
+                        case 5: // 1 - 5                       
+                            for (int i = 0; i < randomIncount; i++)
+                            {
+                                monsters.Add(new Goblin());
+                            } 
+                            break;
+                        case 6:// 1 - 6
+
+                            for (int i = 0; i < randomIncount; i++)
+                            {
+                                monsters.Add(new Goblin());
+                            } 
+                            break;
+
+
                     }
                     break;
                 case 2:
-                    for (int i = 0; i < randomIncount; i++)
+                    switch (minstage)
                     {
-                        monsters.Add(new Goblin());
-                    } // 스테이지 마다 분리해서 
+                        case 0:
+                            //만약 1-0 방이라면~ 몬스터 세팅을 어떻게 
+                            for (int i = 0; i < randomIncount; i++)
+                            {
+                                monsters.Add(new Goblin());
+                            } // 스테이지 마다 분리해서 
+                            break;
+                        case 1: // 1 - 1
+                            for (int i = 0; i < randomIncount; i++)
+                            {
+                                monsters.Add(new Goblin());
+                            } // 스테이지 마다 분리해서 
+                            break;
+                        case 2: // 1 - 2
+                            for (int i = 0; i < randomIncount; i++)
+                            {
+                                monsters.Add(new Goblin());
+                            } // 스테이지 마다 분리해서 
+                            break;
+                        case 3: // 1 - 3                             
+                            for (int i = 0; i < randomIncount; i++)
+                            {
+                                monsters.Add(new Goblin());
+                            } // 스테이지 마다 분리해서 
+                            break;
+                        case 4: // 1 - 4                        
+                            for (int i = 0; i < randomIncount; i++)
+                            {
+                                monsters.Add(new Goblin());
+                            }
+                            break;
+                        case 5: // 1 - 5                       
+                            for (int i = 0; i < randomIncount; i++)
+                            {
+                                monsters.Add(new Goblin());
+                            }
+                            break;
+                        case 6:// 1 - 6
+
+                            for (int i = 0; i < randomIncount; i++)
+                            {
+                                monsters.Add(new Goblin());
+                            }
+                            break;
+                    }
+                    break;
+                case 3:
+                    switch (minstage)
+                    {
+                        case 0:
+                            //만약 1-0 방이라면~ 몬스터 세팅을 어떻게 
+                            for (int i = 0; i < randomIncount; i++)
+                            {
+                                monsters.Add(new Goblin());
+                            } // 스테이지 마다 분리해서 
+                            break;
+                        case 1: // 1 - 1
+                            for (int i = 0; i < randomIncount; i++)
+                            {
+                                monsters.Add(new Goblin());
+                            } // 스테이지 마다 분리해서 
+                            break;
+                        case 2: // 1 - 2
+                            for (int i = 0; i < randomIncount; i++)
+                            {
+                                monsters.Add(new Goblin());
+                            } // 스테이지 마다 분리해서 
+                            break;
+                        case 3: // 1 - 3                             
+                            for (int i = 0; i < randomIncount; i++)
+                            {
+                                monsters.Add(new Goblin());
+                            } // 스테이지 마다 분리해서 
+                            break;
+                        case 4: // 1 - 4                        
+                            for (int i = 0; i < randomIncount; i++)
+                            {
+                                monsters.Add(new Goblin());
+                            }
+                            break;
+                        case 5: // 1 - 5                       
+                            for (int i = 0; i < randomIncount; i++)
+                            {
+                                monsters.Add(new Goblin());
+                            }
+                            break;
+                        case 6:// 1 - 6
+
+                            for (int i = 0; i < randomIncount; i++)
+                            {
+                                monsters.Add(new Goblin());
+                            }
+                            break;
+                    }
                     break;
 
             }
@@ -129,79 +262,62 @@ namespace TextDungeon
 
                 Console.WriteLine("플레이어의 턴!!");
                 Console.WriteLine("원하는 행동을 골라 보세요!");
-                Console.WriteLine("1.공격");
+                Console.WriteLine("1.공격하기");
 
                 switch (CheckValidInput(1, 1))
                 {
                     case 1:
                         Console.WriteLine("원하시는 몬스터를 선택해주세요.");
 
-                        switch (CheckValidInput(1, monsters.Count + 1))
-                        {
-                            case 1: // 공격할 몬스터를 고르기 위한것
-                                Console.WriteLine("고블린 공격!");
-                                monsters[0].Hp -= _player.Atk;
-                                Console.WriteLine("몬스터를 공격합니다.");
-                                Console.WriteLine($"{_player.Atk}에 피해를 주었습니다");
-                                Console.WriteLine($"{monsters[0].Name} 남은 체력 : {monsters[0].Hp}\n");
-                                break;
-                            case 2:
-                                Console.WriteLine("고블린 공격!");
-                                monsters[1].Hp -= _player.Atk;
-                                Console.WriteLine("몬스터를 공격합니다.");
-                                Console.WriteLine($"{_player.Atk}에 피해를 주었습니다");
-                                Console.WriteLine($"{monsters[1].Name} 남은 체력 : {monsters[1].Hp}\n");
-                                break;
-                            case 3:
-                                Console.WriteLine("플레이어의 턴!!");
-                                Console.WriteLine("고블린 공격!");
-                                monsters[2].Hp -= _player.Atk;
-                                Console.WriteLine("몬스터를 공격합니다.");
-                                Console.WriteLine($"{_player.Atk}에 피해를 주었습니다");
-                                Console.WriteLine($"{monsters[2].Name} 남은 체력 : {monsters[2].Hp}\n");
-                                break;
-                            case 4:
-                                Console.WriteLine("플레이어의 턴!!");
-                                Console.WriteLine("고블린 공격!");
-                                monsters[3].Hp -= _player.Atk;
-                                Console.WriteLine("몬스터를 공격합니다.");
-                                Console.WriteLine($"{_player.Atk}에 피해를 주었습니다");
-                                Console.WriteLine($"{monsters[3].Name} 남은 체력 : {monsters[3].Hp}\n");
-                                break;
+                        int selectedMonsterIndex = CheckValidInput(1, monsters.Count) - 1; // monsters의 인덱스는 0부터 시작하기 때문에 -1을 했습니다.
 
+                        Console.WriteLine("{0}를 공격합니다!");
+                        monsters[selectedMonsterIndex].Hp -= _player.Atk;
+                        Console.WriteLine("{0}의 피해를 주었습니다.", _player.Atk);
+                        Console.WriteLine("{0}의 남은체력 :{1}", monsters[selectedMonsterIndex].Name, monsters[selectedMonsterIndex].Hp);
+
+                        if (monsters[selectedMonsterIndex].Hp <= 0)
+                        {
+                            
+                            monsters[selectedMonsterIndex].IsDead = true;
+                            Console.WriteLine("{0}을 무찔렀습니다!\n", monsters[selectedMonsterIndex].Name);
+                            Thread.Sleep(1000);
                         }
                         break;
                 }
                 Thread.Sleep(1000);
 
-                for (int i = 0; i < monsters.Count; i++)
+                foreach (Monster monster in monsters)
                 {
-                    if (monsters[0].Hp <= 0 && monsters[1].Hp <= 0 && monsters[2].Hp <= 0 && monsters[3].Hp <= 0)
+                    if (monster.Hp > 0)
                     {
-                        Thread.Sleep(1000);
-                        monsters[i].IsDead = true;
-                        Console.WriteLine("{0}을 무찔렀습니다!\n", monsters[i].Name);
-                        Console.WriteLine("보상으로 ?골드와 ?을 휙득했습니다.");
+                        Console.Clear();
+                        Console.WriteLine("{0}의 턴!!", monster.Name);
+                        if (!monster.IsDead)
+                        {
+                            _player.Hp -= monster.Atk;
+                            Console.WriteLine($"{monster.Atk}의 피해를 입었습니다\n");
+                            Console.WriteLine($"플레이어의 남은 체력 : {_player.Hp}");
+                            Thread.Sleep(1000);
 
-                        Console.WriteLine("다음 스테이지로 이동합니다....");
-                        mapListIndex++;
+                            if (_player.Hp <= 0)
+                            {
+                                _player.IsDead = true;
+                                Console.WriteLine("플레이어가 사망하였습니다.");
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{monster.Name}은(는) 이미 죽었습니다!");
+                        }
+                        
                     }
                     else
                     {
-                        Console.Clear();
-                        Console.WriteLine("{0}의 턴!!", monsters[i].Name);
-                        _player.Hp -= monsters[i].Atk;
-                        Console.WriteLine($"{monsters[i].Atk}의 피해를 입었습니다\n");
-                        Console.WriteLine($"플레이어의 남은 체력 : {_player.Hp}");
-                        Thread.Sleep(1000);
-
-
-                        if (_player.Hp <= 0)
-                        {
-                            _player.IsDead = true;
-                            Console.WriteLine("플레이어가 사망하였습니다.");
-                        }
+                        Console.WriteLine($"{monster.Name}은(는) 이미 죽었습니다!");
                     }
+
                 }
 
 
@@ -209,7 +325,9 @@ namespace TextDungeon
                 Console.Clear();
             }
 
-            while (!_player.IsDead && !monsters[monstersIndex].IsDead); //조건 정하기
+            while (!_player.IsDead && monsters.Any(monsters => !monsters.IsDead)); 
+            //Any는 LINQ의 기능입니다. 요소가 하나이상이라도 있으면 true을 반환하고, 비어있으면 false를 반환합니다. // monsters의 리스트 에서 isDead가 false인 몬스터가 하나이상 있는지
+            // 확인하려는 용도로 사용했습니다. 즉 살아남은 몬스터가 하나이상 존재한다면 true를 다 죽었다면 false를 반환합니다.
         }
     }
 }
